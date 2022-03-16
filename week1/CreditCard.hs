6aa37974b5f88fff788f5cb9f422176411ea1a6a
Fisher Sun <fisher521.fs@gmail.com>
2022-03-16 15:41:28 -0500

    Update CreditCard

--- week1/CreditCard.hs
+++ week1/CreditCard.hs
@@ -1,3 +1,6 @@
+-- CreditCard.hs from Spring 13
+
+
 -- Convert positive Integers to a list of digits
 -- e.g. 1234 -> [1, 2, 3, 4]
 toDigits :: Integer -> [Integer]
@@ -21,5 +24,21 @@ doubleEveryOther list = doubleEveryOther (take (length list - 2) list) ++ double
 -- If the list has one element, don't double.
 -- Otherwise, double the first element.
 doubleFirst :: [Integer] -> [Integer]
-doubleFirst (first:[]) = [first]
-doubleFirst (first:otherElements) = first * 2 : otherElements
+doubleFirst (firstNumber:[]) = [firstNumber]
+doubleFirst (firstNumber:otherNumbers) = firstNumber * 2 : otherNumbers
+
+
+-- Sum all digits of list
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (firstNumber:otherNumbers)
+  | firstNumber `div` 10 == 0 = firstNumber + sumDigits(otherNumbers)
+  | otherwise = firstNumber `mod` 10 + (firstNumber `div` 10) `mod` 10 + sumDigits(otherNumbers)
+
+-- Validate credit card
+validate :: Integer -> Bool
+validate cardNumber =
+    let result = sumDigits $ doubleEveryOther $ toDigits cardNumber in
+    result `mod` 10 == 0
+
+
