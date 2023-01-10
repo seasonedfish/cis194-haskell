9cfb379deb2b5da09a5d0e297e5bae4d0691aa14
Fisher Sun <f.sun@wustl.edu>
2023-01-10 10:54:30 -0500

    Implement hw4 ex6 fromInteger

--- week4/HW04.hs
+++ week4/HW04.hs
@@ -68,7 +68,7 @@ instance Num a => Num (Poly a) where
     (+) = plus
     (*) = times
     negate      = undefined
-    fromInteger = undefined
+    fromInteger y = P [fromIntegral y]
     -- No meaningful definitions exist
     abs    = undefined
     signum = undefined
